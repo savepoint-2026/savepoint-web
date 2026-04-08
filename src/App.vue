48c8b43d422fd07938f2067c6488a553eb7f871e
nabivi <nerdring54@gmail.com>
2026-04-08 14:37:14 +0900

    design: 페이지 전체 레이아웃 추가

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,30 @@
-<template></template>
-<script setup></script>
+<template>
+  <div class="container">
+    <SideBar></SideBar>
+    <section class="router-container">
+      <router-view></router-view>
+    </section>
+  </div>
+</template>
+
+<script setup>
+import SideBar from './components/common/SideBar.vue'
+</script>
+
+<style>
+.container {
+  width: 100%;
+  height: 100vh;
+  background-color: #f9fafa;
+  padding: 0;
+
+  display: flex;
+}
+
+.router-container {
+  width: calc(100% - 320px);
+  height: 100%;
+  padding: 72px 48px;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/common/SideBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="sidebar"></nav>
+</template>
+
+<script setup></script>
+
+<style>
+.sidebar {
+  width: 320px;
+  height: 100%;
+  background-color: white;
+}
+</style>
