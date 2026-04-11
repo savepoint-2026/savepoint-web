7673bfa484c7a9f27f82e61b73c519659d61a1f8
hurrayPersimmon <rahel337@naver.com>
2026-04-12 01:43:01 +0900

    feat: 그룹 레이스 컴포넌트 분기#38

--- src/pages/GroupRaceView.vue
+++ src/pages/GroupRaceView.vue
@@ -1,24 +1,51 @@
 <template>
-  <!-- <GroupManager></GroupManager> -->
-  <!-- <GroupManager v-if="!groupStore.isGroup" /> -->
-  <!-- <div v-else> -->
-  <RaceTrack />
-  <RankingBoard />
-  <!-- </div> -->
+  <div v-if="!isLoading">
+    <GroupManager
+      v-if="
+        !userStore.userData.groupId ||
+        userStore.userData.groupId === 'empty' ||
+        userStore.userData.groupId === ''
+      "
+    />
+    <div v-else>
+      <RaceTrack />
+      <RankingBoard />
+    </div>
+  </div>
 </template>
 
 <script setup>
 import GroupManager from '@/components/group/GroupManager.vue'
 import RaceTrack from '@/components/group/RaceTrack.vue'
 import RankingBoard from '@/components/group/RankingBoard.vue'
-
+import { useAuthStore } from '@/stores/useAuthStore'
 import { useGroupStore } from '@/stores/useGroupStore'
-import { onMounted } from 'vue'
+import { useTransactionStore } from '@/stores/useTransactionStore'
+import { useUserStore } from '@/stores/useUserStore'
+import { onMounted, ref } from 'vue'
 
+const userStore = useUserStore()
+const AuthStore = useAuthStore()
 const groupStore = useGroupStore()
-const userId = window.sessionStorage.getItem('userId')
+const transactionStore = useTransactionStore()
+const isLoading = ref(true)
 
-onMounted(() => {
-  //   groupStore.fetchMyGroup(userId) //groupId setting
+onMounted(async () => {
+  try {
+    //data loading
+    await userStore.fetchUserData(AuthStore.currentUserId)
+    const groupId = userStore.userData.groupId
+    await groupStore.loadGroup(groupId)
+    const memberIds = groupStore.currentGroup.memberIds
+
+    // memberIds로 transactionStore 접근해서 runners의 지출액 데이터 가져오기
+    const runnersExpenses = await transactionStore.fetchGroupRunners(memberIds)
+    // runners 배열에 userStore에서 userId로 이름, 아바타 이미지 조회해서 추가
+    const runnersInfo = await userStore.fetchUsersInfo(memberIds)
+    // runners 배열에 지출액, 이름, 아바타 이미지 및 필요한 데이터 합치기
+    groupStore.getRunnersDetails(runnersExpenses, runnersInfo)
+  } finally {
+    isLoading.value = false
+  }
 })
 </script>
